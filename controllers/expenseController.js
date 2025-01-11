a5710c7e0e8c7faeaaece7e52f58051b63c031ec
soumen974 <sob99338@gmail.com>
2025-01-11 22:07:18 +0530

    expense back done

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -10,6 +10,50 @@ exports.getExpenses = async (req, res) => {
   }
 };
 
+exports.getExpenseByid = async (req, res) => {
+  try {
+    const expense = await Expense.findOne({ user: req.user ,_id:req.params.id});
+    if (!expense) {
+      return res.status(404).json({ error: 'Expense not found' });
+    }
+    res.json(expense);
+  } catch (err) {
+    //console.error(err.message);
+    //res.status(500).send('Server error');
+    res.status(500).json({ error: `Error : ${err.message}` });
+  }
+};
+
+exports.deleteExpense = async (req, res) => {
+  try {
+    const expense = await Expense.findByIdAndDelete({_id:req.params.id, user: req.user});
+    if(!expense) return res.status(404).json({error:'not found'});
+    res.json({mesg:'Deleted Successfully '});
+
+  } catch (err) {
+    res.status(500).json({erorr:`Error : ${err.message}`});
+  }
+};
+
+exports.updateExpense = async (req, res) => {
+  const { amount, source, date , description , note} = req.body;
+  try {
+    const expense = await Expense.findOneAndUpdate(
+      { _id: req.params.id, user: req.user },
+      { amount, source, date , description , note},
+      { new: true }
+    );
+    if (!expense) {
+      return res.status(404).json({ error: 'Expense not found' });
+    }
+    res.json(expense);
+  } catch (err) {
+    //console.error(err.message);
+    //res.status(500).send('Server error');
+    res.status(500).json({ error: `Error : ${err.message}` });
+  }
+};
+
 exports.addExpense = async (req, res) => {
   const { amount, source, date , description , note} = req.body;
 
--- routes/expenses.js
+++ routes/expenses.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-const { getExpenses, addExpense } = require('../controllers/expenseController');
+const { getExpenses, addExpense ,getExpenseByid,deleteExpense,updateExpense } = require('../controllers/expenseController');
+const { route } = require('./income');
 
 // Get all expenses
 router.get('/', auth, getExpenses);
 
 // Add a new expense
 router.post('/', auth, addExpense);
+
+// Get expense by id
+router.get('/:id', auth, getExpenseByid);
+
+// Delete expense
+router.delete('/:id', auth, deleteExpense);
+
+// Update expense
+router.put('/:id', auth, updateExpense);
+
+
 
 module.exports = router;
