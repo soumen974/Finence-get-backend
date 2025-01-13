dbf0f9d986d1d9a631e38fb6cc86e2b073981e86
soumen974 <sob99338@gmail.com>
2025-01-13 17:54:58 +0530

    add

--- controllers/aggregatedTransactionController.js
+++ controllers/aggregatedTransactionController.js
@@ -3,53 +3,61 @@ const Expense = require('../models/Expense');
 const Income = require('../models/Income');
 
 const getMonthName = (date) => {
-    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    return monthNames[date.getMonth()];
+  const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  return monthNames[date.getMonth()];
 };
 
 exports.aggregateTransactionData = async (req, res) => {
-try {
+  try {
     const incomes = await Income.find({ user: req.user });
     const expenses = await Expense.find({ user: req.user });
 
     const aggregatedData = {};
-    
-    // income calculate
+
+    // Calculate income per month
     incomes.forEach(income => {
-    const month = getMonthName(income.date);
-    if (!aggregatedData[month]) {
+      const month = getMonthName(income.date);
+      if (!aggregatedData[month]) {
         aggregatedData[month] = { incomePerMonth: 0, expensePerMonth: 0 };
-    }
-    aggregatedData[month].incomePerMonth += income.amount;
+      }
+      aggregatedData[month].incomePerMonth += income.amount;
     });
 
-    // expense calculate
+    // Calculate expense per month
     expenses.forEach(expense => {
-    const month = getMonthName(expense.date);
-    if (!aggregatedData[month]) {
+      const month = getMonthName(expense.date);
+      if (!aggregatedData[month]) {
         aggregatedData[month] = { incomePerMonth: 0, expensePerMonth: 0 };
+      }
+      aggregatedData[month].expensePerMonth += expense.amount;
+    });
+
+    // Aggregate the data and update or insert into database
+    for (const month in aggregatedData) {
+      const { incomePerMonth, expensePerMonth } = aggregatedData[month];
+      const netSavingsPerMonth = incomePerMonth - expensePerMonth;
+
+      // Update if exists, otherwise insert
+      await AggregatedTransaction.findOneAndUpdate(
+        { user: req.user, month },
+        { incomePerMonth, expensePerMonth, netSavingsPerMonth },
+        { upsert: true, new: true, setDefaultsOnInsert: true }
+      );
     }
-    aggregatedData[month].expensePerMonth += expense.amount;
-    });
 
-    // net savings calculate and adding to database
-    const aggregatedTransactions = Object.keys(aggregatedData).map(month => {
-    return {
-        user: req.user,
-        month,
-        incomePerMonth: aggregatedData[month].incomePerMonth,
-        expensePerMonth: aggregatedData[month].expensePerMonth,
-        netSavingsPerMonth: aggregatedData[month].incomePerMonth - aggregatedData[month].expensePerMonth
-    };
-    });
+    // Fetch the updated aggregated transactions
+    const aggregatedTransactions = await AggregatedTransaction.find({ user: req.user }).sort({ month: 1 });
 
-    await AggregatedTransaction.insertMany(aggregatedTransactions);
+    // Format the data for the frontend
+    const formattedData = aggregatedTransactions.map((transaction) => ({
+      name: transaction.month,
+      income: transaction.incomePerMonth,
+      expense: transaction.expensePerMonth,
+      Net_Savings: transaction.netSavingsPerMonth,
+    }));
 
-    const LineChartData = await AggregatedTransaction.find({ user: req.user}).sort({ date: -1 , _id: -1 });
-    
-
-    res.status(201).json({ msg: 'Aggregated transaction data fetched successfully' ,LineChartData });
-} catch (error) {
+    res.status(201).json({ msg: 'Aggregated transaction data fetched successfully', LineChartData: formattedData });
+  } catch (error) {
     res.status(500).json({ msg: 'Error aggregating transaction data', error });
-}
+  }
 };
